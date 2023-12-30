02e21541d31728f63b2107ddd15462e7336375b2
Edward Rusli <erusli@sabarjaya.com>
2023-12-30 15:02:48 +0700

    fixed requestFoodList calling repeatedly, reformatted components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,67 @@
+import React, { createContext, useState } from "react";
+import axios from "axios";
 
-import React, {createContext, useState} from 'react'
-import axios from "axios"
-
-import NavBar from './NavBar';
-import SearchBar from './SearchBar';
-import FoodList from './FoodList';
+import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import FoodList from "./FoodList";
 
 export const AppContext = createContext(null);
 
 const App = () => {
+  // const register = (p_username, p_password, p_email) => {
+  // 	axios.post("http://127.0.0.1:8000/api/register/", {
+  // 		"username": p_username,
+  // 		"password": p_password,
+  // 		"email": p_email
+  // 	},{
+  // 		headers: {
+  // 			'Content-Type': 'application/json'
+  // 		}
+  // 	}).then(res => {
+  // 		console.log(res);
+  // 	})
+  // }
 
-	// const register = (p_username, p_password, p_email) => {
-	// 	axios.post("http://127.0.0.1:8000/api/register/", {
-	// 		"username": p_username,
-	// 		"password": p_password,
-	// 		"email": p_email
-	// 	},{
-	// 		headers: {
-	// 			'Content-Type': 'application/json'
-	// 		}
-	// 	}).then(res => {
-	// 		console.log(res);
-	// 	})
-	// }
+  const [shouldRenderFoodList, setShouldRenderFoodList] = useState(false);
+  const [foodList, setFoodList] = useState([]);
 
-	const [shouldRenderFoodList, setShouldRenderFoodList]= useState(false);
-	const [foodList, setFoodList] = useState([]);
+  const requestFoodList = (p_query) => {
+    const params = {
+      apiKey: "840aec0e421d4509aabf9a0eaed9d8ae", // api key dari spoonacular
+      //apiKey: "",
+      query: p_query,
+      addRecipeNutrition: true,
+    };
+    axios
+      .get("https://api.spoonacular.com/recipes/complexSearch", { params })
+      .then((res) => {
+        setFoodList(res.data.results);
+        setShouldRenderFoodList(true);
+        console.log(res.data.results);
+      });
+  };
 
-	const requestFoodList = (p_query) => {
-		const params = {
-			apiKey: "f4bbe7c9c7554d05909632deaa0d4c98", // api key dari spoonacular
-			query: p_query,
-			addRecipeNutrition: true
-		}
-		axios.get("https://api.spoonacular.com/recipes/complexSearch", {params}).then(res => {
-			setFoodList(res.data.results);
-			setShouldRenderFoodList(true);
-			console.log(res.data.results);
-		})
-	}
-
-	return (
-		<AppContext.Provider value={{requestFoodList, foodList}}>
-		<div className='container-fluid'>
-			<div className='row'>
-				<div className='col'><NavBar/></div>
-			</div>
-			<div className='row'>
-				<div className='col'><SearchBar/></div>
-			</div>
-			<div className='row'>
-				<div className='col'>
-					{shouldRenderFoodList ? (<FoodList/>) : (<div></div>)}
-				</div>
-			</div>
-		</div>
-		</AppContext.Provider>
-	)
-}
+  return (
+    <AppContext.Provider value={{ requestFoodList, foodList }}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <NavBar />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <SearchBar />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            {shouldRenderFoodList ? <FoodList /> : <div></div>}
+          </div>
+        </div>
+      </div>
+    </AppContext.Provider>
+  );
+};
 
 export default App;
--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,31 +1,40 @@
-
 import React, { useContext, useState } from "react";
-import {AppContext} from "./App";
+import { AppContext } from "./App";
 
 const SearchBar = () => {
+  const { requestFoodList } = useContext(AppContext);
 
-    const {requestFoodList} = useContext(AppContext);
+  const [query, setQuery] = useState("");
 
-    const [query, setQuery] = useState("");
+  const updateInputValue = (event) => {
+    setQuery(event.target.value);
+  };
 
-    const updateInputValue = (evt) => {
-        setQuery(evt.target.value);
-    }
-
-    return (
-        <div className="SearchBar mt-5">
-            <div className="container-fluid text-center">
-                <div className="row">
-                    <div className="col">
-                        <div className="input-group rounded shadow-sm fs-3">
-                            <input className="form-control" placeholder="Type query" value={query} onChange={evt => updateInputValue(evt)}/>
-                            <button type="button" className="btn btn-outline-primary" onClick={requestFoodList(query)}>Search</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="SearchBar mt-5">
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="col">
+            <div className="input-group rounded shadow-sm fs-3">
+              <input
+                className="form-control"
+                placeholder="Type query"
+                value={query}
+                onChange={(event) => updateInputValue(event)}
+              />
+              <button
+                type="button"
+                className="btn btn-outline-primary"
+                onClick={() => requestFoodList(query)}
+              >
+                Search
+              </button>
             </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
